51946a8e88f668b7d6629d3ef5672fcbb066ddd9
Neku <shinya.komatsu@kingdevice.com>
2016-12-14 15:32:12 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -32,6 +33,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
 
   private
 
