adbf5b522a72ecf609171b20b2f7d6e0c6ac01c8
Neku <shinya.komatsu@kingdevice.com>
2016-12-20 15:55:26 +0000

    modified　gagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       search_word = params[:search].to_s
-      @feed_items = current_user.feed_items.includes(:user).search(search_word).order(created_at: :desc).page(params[:page]).per(1)
+      @feed_items = current_user.feed_items.includes(:user).search(search_word).order(created_at: :desc).page(params[:page]).per(３)
     end
   end
 end
